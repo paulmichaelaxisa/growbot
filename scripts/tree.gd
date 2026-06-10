698eb9f09c1c1f0dc73456ed95baf9b42ad22e25
paulmichaelaxisa <paul.axisa@gmail.com>
2026-06-10 20:23:52 +1000

    seed to sprout automation

---
+++ scripts/tree.gd
@@ -0,0 +1,29 @@
+# scripts/tree.gd
+extends Area2D
+
+@onready var seed_sprite: Sprite2D = $SeedSprite
+@onready var sprout_sprite: Sprite2D = $SproutSprite
+##@onready var grow_sound: AudioStreamPlayer2D = $GrowSound
+
+var is_grown := false
+
+func _ready() -> void:
+	if sprout_sprite:
+		sprout_sprite.scale = Vector2(0.1, 0.1)
+		sprout_sprite.visible = false
+
+func grow() -> void:
+	if is_grown or not sprout_sprite: 
+		return
+	is_grown = true
+	
+	seed_sprite.visible = false
+	sprout_sprite.visible = true
+	
+	var tween = create_tween()
+	tween.tween_property(sprout_sprite, "scale", Vector2(1.0, 1.0), 1.5)
+	tween.set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(sprout_sprite, "position:y", sprout_sprite.position.y - 25, 1.5)
+	
+##	if grow_sound:
+##		grow_sound.play()
